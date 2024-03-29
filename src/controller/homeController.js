e10de2d69005f699a72265df3ff76f612bc0974d
maixuanvien123 <106679180+maixuanvien123@users.noreply.github.com>
2024-03-29 12:11:06 +0700

    update

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,12 @@
+const mysql = require('mysql2');
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt',
+});
+
 const handleHelloword = (req, res) => {
     return res.render("home.ejs");
 }
@@ -8,6 +17,24 @@ const handleUserPage = (req, res) => {
     return res.render("user.ejs");
 }
 
+const handleCreateNewUser = (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let username = req.body.username;
+
+
+    connection.query(
+        ' INSERT INTO users(email, password, username) VALUES(?, ?, ?)', [email, password, username],
+        function (err, results, fields) {
+            if (err) {
+                console.log(err)
+            }
+            console.log(results); // results contains rows returned by server
+        }
+    );
+    return res.send("handleCreateNewUser");
+}
+
 module.exports = {
-    handleHelloword, handleUserPage
+    handleHelloword, handleUserPage, handleCreateNewUser
 }
